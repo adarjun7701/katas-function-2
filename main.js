8be1cfbda4c356389dc72cc3da7b32e19d43c815
adarjun7701 <63601256+adarjun7701@users.noreply.github.com>
2020-06-08 17:42:56 -0400

    commitment

---
+++ main.js
@@ -0,0 +1,66 @@
+// The Katas
+
+// 1 ADD 
+
+function add (x,y) {
+return (x + y)
+}
+console.log(add(2, 4))
+
+
+// 2 Multiply 
+
+function multiply (x,y){
+ let result = 0
+ for ( let index = 0; index < y; index ++) {
+   result = add(result,x)
+ }
+ return result;
+}
+console.log(multiply(6,4))
+
+
+
+ 
+ 
+ // 3 Power/Exponentiation 
+
+ function power (x,n) {
+     let result = 1
+     for (let i = 0; i < n; i++) {
+       result = multiply(result,x)
+     }
+     return result;
+    }
+    console.log(power(2,8))
+ 
+
+
+
+
+// 4  Factorial
+
+function factorial (x){
+   let result = 1
+   for (let i = 1; i <= x; i++){
+       result = multiply(result,i)
+   }
+   return result;
+}
+console.log(factorial(4))
+   
+
+    
+
+
+
+
+// 5 Fibonacci
+
+function fibonacci(result,length){
+    if(result.length >= length){
+        return result;
+    }
+}
+console.log(fibonacci(8))
+
